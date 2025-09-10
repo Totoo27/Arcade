81f62bc1ce877cc611396aae6c12abfc4c739df6
tomy08 <tomasesantacruz@gmail.com>
2025-09-10 11:41:00 -0300

    Add dead menu

--- src/Clases/GamePanel.java
+++ src/Clases/GamePanel.java
@@ -19,253 +19,264 @@ import javax.swing.Timer;
 
 public class GamePanel extends JPanel implements ActionListener, KeyListener {
 
-    // Timers
-    private Timer timer;
-    
-    // Tiempo
-    private int segundos = 0;
-    private int minutos = 0;
-    private long ultimoSegundo = 0;
+	// Timers
+	private Timer timer;
 
-    // Labels
-    private JLabel contadorMonedas;
-    private JLabel tiempo;
-    
-    // TileMap
-    private ArrayList<Tile> tiles = new ArrayList<>();
-    
-    // Proyectiles
-    private ArrayList<Balas> balas = new ArrayList<>();
-    public int BalaJugWidth = 10;
-    
-    // Hitbox
-    private boolean showHitboxes = true;
-    
-    // Gravedad
-    public int gravedad = 1;
-    
-    // Enemigos
-    private ArrayList<Enemigo> EnemigosBasicos = new ArrayList<>();
+	// Tiempo
+	private int segundos = 0;
+	private int minutos = 0;
+	private long ultimoSegundo = 0;
 
-    // Jugador
-    private Player player;
-    private int MonedasJug = 0;
+	// Labels
+	private JLabel contadorMonedas;
+	private JLabel tiempo;
 
-    public GamePanel() {
-        setBackground(new Color(50,50,50));
-        setFocusable(true);
-        
-        // --- Labels ---
-        
-        // ContadorMonedas
-        contadorMonedas = new JLabel("" + MonedasJug);
-        contadorMonedas.setForeground(Color.WHITE); // color del texto
-        contadorMonedas.setFont(GameMain.Pixelart.deriveFont(36f)); // fuente
-        contadorMonedas.setBounds(105, 80, 150, 40); // posición y tamaño
-        
-        // Tiempo
-        tiempo = new JLabel("00:00");
-        tiempo.setForeground(Color.WHITE);
-        tiempo.setFont(GameMain.Pixelart.deriveFont(36f));
-        tiempo.setBounds(860, 20, 200, 40);
-        
-        // Instanciar Jugador
-        player = new Player(200, 200, this);
-        addKeyListener(this); 
-        
-        EnemigosBasicos.add(new Enemigo(400, 400, player));
-        
-        // Toggler de hitboxes
-        getInputMap(WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("H"), "toggleHitboxes");
-        getActionMap().put("toggleHitboxes", new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                showHitboxes = !showHitboxes; // alternar hitboxes
-                repaint();
-            }
-        });
-        
-        tiles.add(new Tile(400, 550, 200, 50, "src/sprites/suelo.png", true));
+	// TileMap
+	private ArrayList<Tile> tiles = new ArrayList<>();
 
-        this.setLayout(null);
-        this.add(contadorMonedas);
-        this.add(tiempo);
-    }
+	// Proyectiles
+	private ArrayList<Balas> balas = new ArrayList<>();
+	public int BalaJugWidth = 10;
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
+	// Hitbox
+	private boolean showHitboxes = true;
 
-        if(player != null) {
-        	
-        	// Dibujar vida del jugador
-        	int coordX = 60;
-        	for(int i = 0; i<player.vida; i++) {
-        		g.drawImage(
-                		new ImageIcon("src/sprites/barravida.png").getImage(),
-                        coordX,
-                        20,
-                        40,
-                        39,
-                        this
-                    );
-        		coordX += 38;
-        	}
-        	
-        	// Dibujar lo que le falta de vida
-        	for(int i = 0; i<player.max_vida - player.vida; i++) {
-        		g.setColor(Color.BLACK);
-        		g.fillRect(coordX, 20, 40, 39);
-        		coordX += 38;
-        	}
-        	
-        	// Sprite Jugador
-        	
-        }
-        
-        // Dibujar Tiles
-        for(Tile tile : tiles) {
-        	tile.draw(g);        	
-        }
-        
-        // Dibujar Monedita
-        
-        g.drawImage(
-        		new ImageIcon("src/sprites/coin.png").getImage(),
-                60,
-                80,
-                37,
-                30,
-                this
-            );
-        
-        // --------------- HITBOXES
-        
-        if(showHitboxes) {
-        	for(Balas bala : balas) {
-            	g.setColor(Color.red);
-            	g.fillRect(bala.x, bala.y, bala.width, bala.height);       	
-            }
-        	
-        	for(Enemigo enemigo : EnemigosBasicos) {
-        		g.setColor(new Color(255,0,0,125));
-            	g.fillRect(enemigo.x, enemigo.y, enemigo.width, enemigo.height); 
-        	}
-        	
-        	if(player != null) {
-        		g.setColor(new Color(0, 255, 255, 125));
-                g.fillRect(player.x, player.y, player.width, player.height);
-        	}
-        	
-        }
-        repaint();
-    }
+	// Gravedad
+	public int gravedad = 1;
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-    	
-    	// Tiempo juego
-    	if(System.currentTimeMillis() >= ultimoSegundo + 1000) {
-    		segundos++;
-    		ultimoSegundo = System.currentTimeMillis();
-        	if (segundos == 60) {
-                segundos = 0;
-                minutos++;
-        	}
-    	}
-        String tiempoTexto = String.format("%02d:%02d", minutos, segundos);
-        tiempo.setText(tiempoTexto);
-        
-    	
-        // Movimiento Jugador
-        if(player != null) {
-            player.move(this.getWidth(), this.getHeight(), gravedad, tiles);
-        }
-        
-        // Movimiento Balas
-        for(Balas bala : balas) {
-        	bala.move();
-        }
-        
-        // Movimiento y Colision Enemigos
-        for(Enemigo enemigo : EnemigosBasicos) {
-        	enemigo.movimiento(getHeight(), gravedad);
-        	
-        	// Colision con jugador
-        	if(enemigo.getBounds().intersects(player.getBounds())) {
-        		player.RecibirHit();
-        	}
-        }
-        
-        // Enemigos Recibiendo Golpe
-        
-        for(int i = 0; i<balas.size();i++) {
-        	Balas bala = balas.get(i);
-        	boolean impactada = false;
-        	
-        	for(Enemigo enemigo : EnemigosBasicos){
-        		
-        		if(bala.getBounds().intersects(enemigo.getBounds())) {
-        			enemigo.recibirGolpe();
-        			balas.remove(i);
-        			impactada = true;
-            		break;
-        		}
-        		
-        	}
-        	
-        	if(impactada) { i--; continue;}
-        	
-        }
-        
-        // Muerte Enemigos
-        
-        for(int i = 0; i<EnemigosBasicos.size(); i++) {
-        	Enemigo enemigo = EnemigosBasicos.get(i);
-        	
-        	if(enemigo.vida <= 0) {
-        		MonedasJug += enemigo.monedas;
-        		EnemigosBasicos.remove(i);
-        	}
-        }
-        
-        // Escribir monedas
-        contadorMonedas.setText("" + MonedasJug);
-    }
+	// Enemigos
+	private ArrayList<Enemigo> EnemigosBasicos = new ArrayList<>();
 
-    public void iniciarJuego() {
-        if (timer == null) {
-            timer = new Timer(16, this); // Masomenos 60fps
-            requestFocusInWindow();
-        }
-        timer.start();
-    }
-    
-    public void disparoJugador(int x,int y, int direccion) {
-    	int velocidad = 15;
-    	int height = 10;
-    	
-    	balas.add(new Balas(x, y, BalaJugWidth, height, velocidad, direccion));
-    }
-    
-    // --- Implementación del KeyListener en GamePanel ---
-    // El GamePanel delega los eventos de teclado al objeto Player
-    @Override
-    public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_LEFT) player.leftPressed = true;
-        if (e.getKeyCode() == KeyEvent.VK_RIGHT) player.rightPressed = true;
-        if (e.getKeyCode() == KeyEvent.VK_SPACE) player.spacePressed = true;
-        if (e.getKeyCode() == KeyEvent.VK_SHIFT) player.disparo = true;
-    }
+	// Jugador
+	private Player player;
+	private int MonedasJug = 0;
 
-    @Override
-    public void keyReleased(KeyEvent e) {
-    	if (e.getKeyCode() == KeyEvent.VK_LEFT) player.leftPressed = false;
-        if (e.getKeyCode() == KeyEvent.VK_RIGHT) player.rightPressed = false;
-        if (e.getKeyCode() == KeyEvent.VK_SPACE) player.spacePressed = false;
-        if (e.getKeyCode() == KeyEvent.VK_SHIFT) player.disparo = false;
-    }
+	public GamePanel() {
+		setBackground(new Color(50, 50, 50));
+		setFocusable(true);
 
-    @Override
-    public void keyTyped(KeyEvent e) {}
+		// --- Labels ---
+
+		// ContadorMonedas
+		contadorMonedas = new JLabel("" + MonedasJug);
+		contadorMonedas.setForeground(Color.WHITE); // color del texto
+		contadorMonedas.setFont(GameMain.Pixelart.deriveFont(36f)); // fuente
+		contadorMonedas.setBounds(105, 80, 150, 40); // posición y tamaño
+
+		// Tiempo
+		tiempo = new JLabel("00:00");
+		tiempo.setForeground(Color.WHITE);
+		tiempo.setFont(GameMain.Pixelart.deriveFont(36f));
+		tiempo.setBounds(860, 20, 200, 40);
+
+		// Instanciar Jugador
+		player = new Player(200, 200, this);
+		addKeyListener(this);
+
+		EnemigosBasicos.add(new Enemigo(400, 400, player));
+
+		// Toggler de hitboxes
+		getInputMap(WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("H"), "toggleHitboxes");
+		getActionMap().put("toggleHitboxes", new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				showHitboxes = !showHitboxes; // alternar hitboxes
+				repaint();
+			}
+		});
+
+		tiles.add(new Tile(400, 550, 200, 50, "src/sprites/suelo.png", true));
+
+		this.setLayout(null);
+		this.add(contadorMonedas);
+		this.add(tiempo);
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		if (player != null) {
+
+			// Dibujar vida del jugador
+			int coordX = 60;
+			for (int i = 0; i < player.vida; i++) {
+				g.drawImage(new ImageIcon("src/sprites/barravida.png").getImage(), coordX, 20, 40, 39, this);
+				coordX += 38;
+			}
+
+			// Dibujar lo que le falta de vida
+			for (int i = 0; i < player.max_vida - player.vida; i++) {
+				g.setColor(Color.BLACK);
+				g.fillRect(coordX, 20, 40, 39);
+				coordX += 38;
+			}
+
+			// Sprite Jugador
+
+		}
+
+		// Dibujar Tiles
+		for (Tile tile : tiles) {
+			tile.draw(g);
+		}
+
+		// Dibujar Monedita
+
+		g.drawImage(new ImageIcon("src/sprites/coin.png").getImage(), 60, 80, 37, 30, this);
+
+		// --------------- HITBOXES
+
+		if (showHitboxes) {
+			for (Balas bala : balas) {
+				g.setColor(Color.red);
+				g.fillRect(bala.x, bala.y, bala.width, bala.height);
+			}
+
+			for (Enemigo enemigo : EnemigosBasicos) {
+				g.setColor(new Color(255, 0, 0, 125));
+				g.fillRect(enemigo.x, enemigo.y, enemigo.width, enemigo.height);
+			}
+
+			if (player != null) {
+				g.setColor(new Color(0, 255, 255, 125));
+				g.fillRect(player.x, player.y, player.width, player.height);
+			}
+
+		}
+		repaint();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		// Tiempo juego
+		if (System.currentTimeMillis() >= ultimoSegundo + 1000) {
+			segundos++;
+			ultimoSegundo = System.currentTimeMillis();
+			if (segundos == 60) {
+				segundos = 0;
+				minutos++;
+			}
+		}
+		String tiempoTexto = String.format("%02d:%02d", minutos, segundos);
+		tiempo.setText(tiempoTexto);
+
+		// Movimiento Jugador
+		if (player != null) {
+			player.move(this.getWidth(), this.getHeight(), gravedad, tiles);
+		}
+
+		// Movimiento Balas
+		for (Balas bala : balas) {
+			bala.move();
+		}
+
+		// Movimiento y Colision Enemigos
+		for (Enemigo enemigo : EnemigosBasicos) {
+			enemigo.movimiento(getHeight(), gravedad);
+
+			// Colision con jugador
+			if (enemigo.getBounds().intersects(player.getBounds())) {
+				player.RecibirHit();
+			}
+		}
+
+		// Enemigos Recibiendo Golpe
+
+		for (int i = 0; i < balas.size(); i++) {
+			Balas bala = balas.get(i);
+			boolean impactada = false;
+
+			for (Enemigo enemigo : EnemigosBasicos) {
+
+				if (bala.getBounds().intersects(enemigo.getBounds())) {
+					enemigo.recibirGolpe();
+					balas.remove(i);
+					impactada = true;
+					break;
+				}
+
+			}
+
+			if (impactada) {
+				i--;
+				continue;
+			}
+
+		}
+
+		// Muerte Enemigos
+
+		for (int i = 0; i < EnemigosBasicos.size(); i++) {
+			Enemigo enemigo = EnemigosBasicos.get(i);
+
+			if (enemigo.vida <= 0) {
+				MonedasJug += enemigo.monedas;
+				EnemigosBasicos.remove(i);
+			}
+		}
+
+		// Escribir monedas
+		contadorMonedas.setText("" + MonedasJug);
+	}
+
+	public void iniciarJuego() {
+		if (timer == null) {
+			timer = new Timer(16, this); // Masomenos 60fps
+			requestFocusInWindow();
+		}
+
+		// Reiniciar el estado del juego
+		segundos = 0;
+		minutos = 0;
+		ultimoSegundo = System.currentTimeMillis();
+		MonedasJug = 0;
+		contadorMonedas.setText("" + MonedasJug);
+		balas.clear();
+		EnemigosBasicos.clear();
+		EnemigosBasicos.add(new Enemigo(400, 400, player)); // Reiniciar enemigos
+		player.x = 200; // Coordenada inicial X
+		player.y = 200; // Coordenada inicial Y
+		player.vida = player.max_vida; // Restaurar la vida del jugador
+
+		timer.start();
+	}
+
+	public void disparoJugador(int x, int y, int direccion) {
+		int velocidad = 15;
+		int height = 10;
+
+		balas.add(new Balas(x, y, BalaJugWidth, height, velocidad, direccion));
+	}
+
+	// --- Implementación del KeyListener en GamePanel ---
+	// El GamePanel delega los eventos de teclado al objeto Player
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_LEFT)
+			player.leftPressed = true;
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+			player.rightPressed = true;
+		if (e.getKeyCode() == KeyEvent.VK_SPACE)
+			player.spacePressed = true;
+		if (e.getKeyCode() == KeyEvent.VK_SHIFT)
+			player.disparo = true;
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_LEFT)
+			player.leftPressed = false;
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+			player.rightPressed = false;
+		if (e.getKeyCode() == KeyEvent.VK_SPACE)
+			player.spacePressed = false;
+		if (e.getKeyCode() == KeyEvent.VK_SHIFT)
+			player.disparo = false;
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+	}
 }
--- src/Clases/Player.java
+++ src/Clases/Player.java
@@ -3,141 +3,163 @@ package Clases;
 import java.awt.Rectangle;
 import java.util.ArrayList;
 
+import javax.swing.SwingUtilities;
+
 public class Player {
-    
-    public int x,y;
-    public int width = 40;
-    public int height = 80;
-    private GamePanel panel;
-    
-    // Velocidad
-    public int dx = 0;
-    public int dy = 0;
-    private int speed = 10;
-    
-    // Disparo
-    private int direccion = 1;
-    private int margenDisparo;
-    private long LastAttackTime;
-    
-    // Vida
-    public int max_vida = 4;
-    public int vida = max_vida;
-    private boolean inmunidad = false;
-    private long LastHitTime;
-    
-    // Movimientos
-    public boolean leftPressed = false;
-    public boolean rightPressed = false;
-    public boolean spacePressed = false;
-    public boolean disparo = false;
-    public boolean tocandoPiso = false;
-    
-    public Player(int startX, int startY, GamePanel panel) { 
-        this.x = startX;
-        this.y = startY;
-        this.panel = panel;
-    }
-    
-    // Mover
-    public void move(int panelWidth, int panelHeight, int gravedad, ArrayList<Tile> tiles) {
-    	dx = 0;
-        if (leftPressed) {
-        	dx = -speed;
-        	direccion = -1;
-        	margenDisparo = -panel.BalaJugWidth; // Width de las balas del jugador
-        	
-        }
-        if (rightPressed) {
-        	dx = speed;
-        	direccion = 1;
-        	margenDisparo = width;
-        }
-        if (spacePressed && tocandoPiso) {
-        	y -= 5;
-        	dy = -17;
-        }
-        if(disparo) {
-        	Disparar();
-        }
 
-        x += dx;
-        if (x < 0) x = 0;
-        if (x + width > panelWidth) x = panelWidth - width;
+	public int x, y;
+	public int width = 40;
+	public int height = 80;
+	private GamePanel panel;
 
-        if(y + height >= panelHeight) {
-            dy = 0;
-            y = panelHeight - height;
-            tocandoPiso = true;
-        } else {
-        	tocandoPiso = false;
-            dy += gravedad;
-        }
-        
-        // Colision con los Tiles
-        // Colisión con los Tiles en el eje X
-        for (Tile tile : tiles) {
-            if (this.getBounds().intersects(tile.getBounds()) && tile.isSolid()) {
-                if (this.dx > 0) { // Si el jugador se mueve a la derecha
-                    this.x = tile.getX() - this.width;
-                    this.dx = 0;
-                } else if (this.dx < 0) { // Si el jugador se mueve a la izquierda
-                    this.x = tile.getX() + tile.getWidth();
-                    this.dx = 0;
-                }
-            }
-        }
+	// Velocidad
+	public int dx = 0;
+	public int dy = 0;
+	private int speed = 10;
 
-        // Movimiento y colisión en el eje Y
-        this.y += this.dy;
-        
-        // Colisión con los Tiles en el eje Y
-        for (Tile tile : tiles) {
-            if (this.getBounds().intersects(tile.getBounds()) && tile.isSolid()) {
-                if (this.dy > 0) { // Si el jugador cae
-                    this.y = tile.getY() - this.height;
-                    this.dy = 0;
-                    this.tocandoPiso = true;
-                } else if (this.dy < 0) { // Si el jugador choca por debajo
-                    this.y = tile.getY() + tile.getHeight();
-                    this.dy = 0;
-                }
-            }
-        }
-    }
-    
-    // Bajar la vida y eso
-    public void RecibirHit() {
-    	if(!inmunidad) {
-    		inmunidad = true;
-    		vida--;
-    		if(vida <= 0) {
-    			vida = 0;
-    		}
-    		// Reproducir Sonido
-    		LastHitTime = System.currentTimeMillis();
-    	}
-    	
-    	// Después de un tiempito se le va la inmunidad
-    	if(System.currentTimeMillis() >= LastHitTime + 500) {
-    		LastHitTime = System.currentTimeMillis();
-    		inmunidad = false;
-    	}
-    	
-    }
-    
-    // Disparar (un toque obvio)
-    private void Disparar() {
-    	
-    	if(System.currentTimeMillis() >= LastAttackTime + 500) {
-    		panel.disparoJugador(x + margenDisparo, y + height/3, direccion);
-    		LastAttackTime = System.currentTimeMillis();
-    	}
-    		
-    }
-    
-    public Rectangle getBounds() {
-        return new Rectangle(x, y, width, height);
-    }
-    
+	// Disparo
+	private int direccion = 1;
+	private int margenDisparo;
+	private long LastAttackTime;
+
+	// Vida
+	public int max_vida = 4;
+	public int vida = max_vida;
+	private boolean inmunidad = false;
+	private long LastHitTime;
+
+	// Movimientos
+	public boolean leftPressed = false;
+	public boolean rightPressed = false;
+	public boolean spacePressed = false;
+	public boolean disparo = false;
+	public boolean tocandoPiso = false;
+
+	public Player(int startX, int startY, GamePanel panel) {
+		this.x = startX;
+		this.y = startY;
+		this.panel = panel;
+	}
+
+	// Mover
+	public void move(int panelWidth, int panelHeight, int gravedad, ArrayList<Tile> tiles) {
+		dx = 0;
+		if (leftPressed) {
+			dx = -speed;
+			direccion = -1;
+			margenDisparo = -panel.BalaJugWidth; // Width de las balas del jugador
+
+		}
+		if (rightPressed) {
+			dx = speed;
+			direccion = 1;
+			margenDisparo = width;
+		}
+		if (spacePressed && tocandoPiso) {
+			y -= 5;
+			dy = -17;
+		}
+		if (disparo) {
+			Disparar();
+		}
+
+		x += dx;
+		if (x < 0)
+			x = 0;
+		if (x + width > panelWidth)
+			x = panelWidth - width;
+
+		if (y + height >= panelHeight) {
+			dy = 0;
+			y = panelHeight - height;
+			tocandoPiso = true;
+		} else {
+			tocandoPiso = false;
+			dy += gravedad;
+		}
+
+		// Colision con los Tiles
+		// Colisión con los Tiles en el eje X
+		for (Tile tile : tiles) {
+			if (this.getBounds().intersects(tile.getBounds()) && tile.isSolid()) {
+				if (this.dx > 0) { // Si el jugador se mueve a la derecha
+					this.x = tile.getX() - this.width;
+					this.dx = 0;
+				} else if (this.dx < 0) { // Si el jugador se mueve a la izquierda
+					this.x = tile.getX() + tile.getWidth();
+					this.dx = 0;
+				}
+			}
+		}
+
+		// Movimiento y colisión en el eje Y
+		this.y += this.dy;
+
+		// Colisión con los Tiles en el eje Y
+		for (Tile tile : tiles) {
+			if (this.getBounds().intersects(tile.getBounds()) && tile.isSolid()) {
+				if (this.dy > 0) { // Si el jugador cae
+					this.y = tile.getY() - this.height;
+					this.dy = 0;
+					this.tocandoPiso = true;
+				} else if (this.dy < 0) { // Si el jugador choca por debajo
+					this.y = tile.getY() + tile.getHeight();
+					this.dy = 0;
+				}
+			}
+		}
+	}
+
+	// Bajar la vida y eso
+	public void RecibirHit() {
+		if (!inmunidad) {
+			inmunidad = true;
+			vida--;
+			if (vida <= 0) {
+				vida = 0;
+				morir();
+			}
+			// Reproducir Sonido
+			LastHitTime = System.currentTimeMillis();
+		}
+
+		// Después de un tiempito se le va la inmunidad
+		if (System.currentTimeMillis() >= LastHitTime + 500) {
+			LastHitTime = System.currentTimeMillis();
+			inmunidad = false;
+		}
+
+	}
+
+	private void morir() {
+		// Mostrar el menú de muerte
+		GameMain gameMain = (GameMain) SwingUtilities.getWindowAncestor(panel);
+		gameMain.mostrarMenuMuerte();
+
+		// Reiniciar el jugador después de morir
+		SwingUtilities.invokeLater(() -> {
+			this.x = 200; // Coordenada inicial X
+			this.y = 200; // Coordenada inicial Y
+			this.vida = max_vida; // Restaurar la vida máxima
+			this.dx = 0;
+			this.dy = 0;
+			this.inmunidad = false;
+			this.tocandoPiso = false;
+		});
+	}
+
+	// Disparar (un toque obvio)
+	private void Disparar() {
+
+		if (System.currentTimeMillis() >= LastAttackTime + 500) {
+			panel.disparoJugador(x + margenDisparo, y + height / 3, direccion);
+			LastAttackTime = System.currentTimeMillis();
+		}
+
+	}
+
+	public Rectangle getBounds() {
+		return new Rectangle(x, y, width, height);
+	}
+
 }
